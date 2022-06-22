f989dd99b797fb1e6781d77fdb075a3bee140592
Abderrahmane Ettounani <tounaniabderrahmane00@gmail.com>
2022-06-22 13:07:54 +0100

    finish 80% in the project

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -3,21 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:scanner/widget/CounterStorage.widget.dart';
 import 'dart:io';
- 
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key, required this.storage});
-  @override
-  State<Home> createState() => _HomeState();
   final CounterStorage storage;
+  @override
+  _HomeState createState() => _HomeState();
+
 }
 
 class _HomeState extends State<Home> {
-  String valueScan="";
-  String s="";
+  String gloabalContent = "";
+  String existNotExist = "";
   void _readFile() {
     widget.storage.readCounter().then((value) {
-       setState(() {
-         s = value;
+      setState(() {
+        gloabalContent = value;
       });
     });
   }
@@ -30,98 +32,120 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Thalès Scanner"),
-          centerTitle: true,
-        ),
-
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(s),
-              Material(
-                color: Colors.orange,
-                elevation: 8,
-                borderRadius: BorderRadius.circular(10),
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: InkWell(
-                  splashColor: Colors.black26,
-                  onTap: (){
-                    scanMe();
-                    (valueScan !="")?_writeFile(valueScan):valueScan;
-
-                    //Navigator.pushNamed(context, "/scanner");
-                  },
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Ink.image(
-                        image: AssetImage('images/scanner.png'),
-                        height: 120,
-                        width: 120,
-                        fit: BoxFit.cover,
-                      ),
-                      SizedBox(height: 6,),
-                      Text("Scanner",style: TextStyle(fontSize: 32,color: Colors.white),),
-                      SizedBox(height: 6,)
-                    ],
+      appBar: AppBar(
+        title: Text("Thalès Scanner"),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          Text(
+            existNotExist,
+            style: Theme.of(context).textTheme.headline3,
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Material(
+                  color: Colors.orange,
+                  elevation: 8,
+                  borderRadius: BorderRadius.circular(10),
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: InkWell(
+                    splashColor: Colors.black26,
+                    onTap: () {
+                      scanMe(); // scan the barcode
+                      //Navigator.pushNamed(context, "/scanner");
+                    },
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Ink.image(
+                          image: AssetImage('images/scanner.png'),
+                          height: 120,
+                          width: 120,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        Text(
+                          "Scanner",
+                          style: TextStyle(fontSize: 32, color: Colors.white),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Material(
-                color: Colors.orange,
-                elevation: 8,
-                borderRadius: BorderRadius.circular(10),
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: InkWell(
-                  splashColor: Colors.black26,
-                  onTap: (){
-                    _readFile();
-                    //Navigator.pushNamed(context, "/importer");
-                  },
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Ink.image(
-                        image: AssetImage('images/sc.jpg'),
-                        height: 120,
-                        width: 120,
-                        fit: BoxFit.cover,
-                      ),
-                      SizedBox(height: 6,),
-                      Text("Importer",style: TextStyle(fontSize: 32,color: Colors.white),),
-                      SizedBox(height: 6,)
-                    ],
+                Material(
+                  color: Colors.orange,
+                  elevation: 8,
+                  borderRadius: BorderRadius.circular(10),
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: InkWell(
+                    splashColor: Colors.black26,
+                    onTap: () {
+                      _readFile();
+                      setState(() {
+                        existNotExist = gloabalContent;
+                      });
+                      //Navigator.pushNamed(context, "/importer");
+                    },
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Ink.image(
+                          image: AssetImage('images/sc.jpg'),
+                          height: 120,
+                          width: 120,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        Text(
+                          "Importer",
+                          style: TextStyle(fontSize: 32, color: Colors.white),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
-          )
-        ),
+              ],
+            ),
+          ),
+        ],
+      )),
     );
-
   }
 
-  Future<void> scanMe  () async{
-      try{
-       await FlutterBarcodeScanner.scanBarcode(
-            "#2A99CF",
-            "cancel",
-            true,
-            ScanMode.BARCODE).then((value){
-               setState((){
-                valueScan=value;
-              });
+  Future<void> scanMe() async {
+    String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        "#2A79CF", "cancel", true, ScanMode.BARCODE);
+    _readFile(); //read all the content of the file
+    if (barcodeScanRes != "") {
+      var array = gloabalContent.split(';'); //split the global data in the file
 
-
-              FutureOr<String> f="";
-               return f;
-            });
-
-      }catch(e){
-        rethrow;
+      if (array.contains(barcodeScanRes)) {
+        FlutterRingtonePlayer.play(fromAsset: "assets/1.mp3");
+        setState(() {
+          existNotExist = "exist ";
+        });
+      } else {
+        FlutterRingtonePlayer.play(fromAsset: "assets/2.mp3");
+        gloabalContent="$gloabalContent;$barcodeScanRes";
+        setState(() {
+          existNotExist = "Not exist ";
+        });
+        _writeFile(gloabalContent);
       }
+    }
   }
-
 }
-
--- lib/widget/CounterStorage.widget.dart
+++ lib/widget/CounterStorage.widget.dart
@@ -11,25 +11,22 @@ class CounterStorage {
 
   Future<File> get _localFile async {
     final path = await _localPath;
-    return File('$path/scanner.txt');
+    return File('$path/scanner.csv');
   }
 
   Future<String> readCounter() async {
-    try {
+
       final file = await _localFile;
       // Read the file
       final contents = await file.readAsString();
 
       return contents;
-    } catch (e) {
-      // If encountering an error, return 0
-      return "";
-    }
+
   }
 
   Future<File> writeCounter(String counter) async {
     final file = await _localFile;
     // Write the file
-    return file.writeAsString("$counter\n",mode: FileMode.append);
+    return file.writeAsString(counter,flush: false);
   }
 }
